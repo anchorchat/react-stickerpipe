8fb1de7d7243424fe96e54aae17d1dc1de6d3c18
sjaakluthart <mail@sjaakluthart.nl>
2016-11-22 16:55:57 +0100

    Added sticker client to component.

--- src/StickerList.jsx
+++ src/StickerList.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import StickerPipeClient from './client';
 
 class StickerList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.client = new StickerPipeClient(props.apiKey, props.userId, 'https://api.stickerpipe.com/api/v2');
+  }
+
   render() {
     return(<h1>StickerList</h1>);
   }
 }
+
+StickerList.propTypes = {
+  apiKey: React.PropTypes.string.isRequired,
+  userId: React.PropTypes.string.isRequired
+};
 
 export default StickerList;
