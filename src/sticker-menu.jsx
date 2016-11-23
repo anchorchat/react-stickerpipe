89a6c669daf2cca298f57436fad6e3a9190ac34b
Sjaak Luthart <mail@sjaakluthart.nl>
2016-11-23 14:21:46 +0100

    Very basic setup for rendering stickers.

--- src/sticker-menu.jsx
+++ src/sticker-menu.jsx
@@ -2,14 +2,56 @@ import React, { Component } from 'react';
 import StickerPipeClient from './client';
 
 class StickerMenu extends Component {
+  static renderList(stickers) {
+    return (
+      <div>
+        {stickers.map(sticker => <img key={sticker.pack_name} src={sticker.main_icon.xhdpi} role="presentation" />)}
+      </div>
+    );
+  }
+
   constructor(props) {
     super(props);
 
     this.client = new StickerPipeClient(props.apiKey, props.userId, 'https://api.stickerpipe.com/api/v2');
+
+    this.state = {
+      loading: true,
+      stickers: []
+    };
+  }
+
+  componentWillMount() {
+    this.client.getShop((err, res) => {
+      if (err) {
+        console.log(err);
+
+        return false;
+      }
+
+      const response = JSON.parse(res.text);
+      console.log(response);
+
+      this.setState({
+        stickers: response.data,
+        loading: false
+      });
+
+      return false;
+    });
   }
 
   render() {
-    return <h1>StickerMenu</h1>;
+    return (
+      <div>
+        <h1>StickerMenu</h1>
+        {
+          !this.state.loading
+          ? this.constructor.renderList(this.state.stickers)
+          : <p>Loading...</p>
+        }
+      </div>
+    );
   }
 }
 
