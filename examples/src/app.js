bea8c131a0c139ccfb2919be94c058aa011a1380
Sjaak Luthart <mail@sjaakluthart.nl>
2016-11-23 11:22:41 +0100

    Added examples setup.

---
+++ examples/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+import StickerList from '../../lib/index';
+
+function App() {
+  return (
+    <StickerList apiKey="bla" userId="test" />
+  );
+}
+
+export default App;
