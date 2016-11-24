be359bb20942b967070b664786bbb36d954d1b8b
Sjaak Luthart <mail@sjaakluthart.nl>
2016-11-24 10:18:40 +0100

    Added sendSticker to example.

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import StickerMenu from '../../lib/index';
 import settings from './settings.json';
 
-function App() {
-  return (
-    <section className="demo">
-      <h1>
-        <a
-          href="https://github.com/anchorchat/react-stickerpipe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React StickerPipe
-        </a>
-      </h1>
-      <StickerMenu apiKey={settings.apiKey} userId={settings.userId} />
-    </section>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sticker: null
+    };
+
+    this.sendSticker = this.sendSticker.bind(this);
+  }
+
+  sendSticker(sticker) {
+    console.log(sticker);
+    this.setState({
+      sticker
+    });
+  }
+
+  render() {
+    const { sticker } = this.state;
+
+    return (
+      <section className="demo">
+        <h1>
+          <a
+            href="https://github.com/anchorchat/react-stickerpipe"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React StickerPipe
+          </a>
+        </h1>
+        {sticker ? <img src={sticker.image.mdpi} alt="sticker" /> : null}
+        <StickerMenu
+          apiKey={settings.apiKey}
+          userId={settings.userId}
+          sendSticker={this.sendSticker}
+        />
+      </section>
+    );
+  }
 }
 
 export default App;
