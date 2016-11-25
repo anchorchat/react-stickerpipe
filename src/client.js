b8c9f38fcefbfa39ebf9bfc0388749038d24cf84
RijkvanZanten <rijkvanzanten@me.com>
2016-11-25 22:06:19 +0100

    make requestmethod a separate function

--- src/client.js
+++ src/client.js
@@ -1,40 +1,40 @@
 import request from 'superagent';
 
+function performRequest(url, method, callback) {
+  const options = {
+    url,
+    headers: {
+      ApiKey: this.key,
+      Platform: 'JS',
+      UserId: this.userId
+    }
+  };
+
+  request
+    [method](options.url)
+    .set(options.headers)
+    .end(callback);
+}
+
 class StickerPipeClient {
   constructor(key, userId, baseUrl) {
     Object.assign(this, { key, userId, baseUrl });
   }
 
-  _performRequest(url, method, callback) {
-    const options = {
-      url,
-      headers: {
-        ApiKey: this.key,
-        Platform: 'JS',
-        UserId: this.userId
-      }
-    };
-
-    request
-      [method](options.url)
-      .set(options.headers)
-      .end(callback);
-  }
-
   getMyPacks(callback) {
-    this._performRequest(`${this.baseUrl}/shop/my`, 'get', callback);
+    performRequest(`${this.baseUrl}/shop/my`, 'get', callback);
   }
 
   getShop(callback) {
-    this._performRequest(`${this.baseUrl}/shop`, 'get', callback);
+    performRequest(`${this.baseUrl}/shop`, 'get', callback);
   }
 
   getPackPreview(packName, callback) {
-    this._performRequest(`${this.baseUrl}/packs/${packName}`, 'get', callback);
+    performRequest(`${this.baseUrl}/packs/${packName}`, 'get', callback);
   }
 
   purchasePack(packName, callback) {
-    this._performRequest(`${this.baseUrl}/packs/${packName}`, 'post', callback);
+    performRequest(`${this.baseUrl}/packs/${packName}`, 'post', callback);
   }
 }
 
