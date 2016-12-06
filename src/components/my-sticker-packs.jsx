27d0ab0d15f07fcb4b177eabf002c26388a29e98
RijkvanZanten <rijkvanzanten@me.com>
2016-12-06 17:36:22 +0100

    :art: move icons to separate folder

--- src/components/my-sticker-packs.jsx
+++ src/components/my-sticker-packs.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import StickerPack from './sticker-pack';
 import Sticker from './sticker';
 import parseResponse from '../parse-response';
-import IconAdd from './icon-add';
+import IconAdd from './icons/icon-add';
 
 class MyStickerPacks extends Component {
   static propTypes = {
